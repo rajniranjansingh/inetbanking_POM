37b3c79e77874230e9b0ba0298da58673a20b77a
rajniranjansingh <rajni.ranjansingh4@gmail.com>
2020-04-02 14:42:52 +0530

    Updated with API Test integration

--- src/test/java/com/inetbanking/testCases/TC_LoginTest.java
+++ src/test/java/com/inetbanking/testCases/TC_LoginTest.java
@@ -1,10 +1,15 @@
 package com.inetbanking.testCases;
 
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 import java.io.IOException;
 
 import org.testng.Assert;
+import org.testng.ITestResult;
 
 import com.inetbanking.pageObjects.LoginPage;
 
@@ -32,6 +37,29 @@ public class TC_LoginTest extends BaseClass{
 			capturescreen(driver, "loginTest");
 			Assert.assertTrue(false);
 			logger.error("Login Test failed");
+		}
+	}
+	
+	@AfterMethod
+	public void writeResult(ITestResult result)
+	{
+		System.out.println("Result of Login Test after execution is : "+result.getStatus());
+		if(result.getStatus() == 1)
+		{
+			given().log().all()
+			.when()
+			.get("http://restapi.demoqa.com/utilities/weather/city/Pune")
+			.then()
+			.statusCode(200)
+			.statusLine("HTTP/1.1 200 OK")
+			.assertThat()
+			.body("City", equalTo("Pune"))
+			.header("content-type", "application/json").log().all();
+			System.out.println("PASSED!!!!!!!!!!!!!!!!!!!!!");
+		}
+		else
+		{
+			System.out.println("FAILED!!!!!!!!!!!!!!!!!!!!!");
 		}
 	}
 }
